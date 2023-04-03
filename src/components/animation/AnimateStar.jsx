d8eff4d4bb99140dfab9c1b0f891d6718bf75f83
AnarMikailov <a.v.mikayilov@gmail.com>
2023-04-03 17:56:40 +0400

    about

---
+++ src/components/animation/AnimateStar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./AnimateStar.scss";
+const AnimateStar = () => {
+  return (
+    <div>
+      <div class="page-bg"></div>
+
+      <div class="animation-wrapper">
+        <div class="particle particle-1"></div>
+      </div>
+    </div>
+  );
+};
+
+export default AnimateStar;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import Sidebar from '../components/Sidebar';
-import './About.scss';
-import AnimatedLetters from '../components/animation/AnimatedLetters';
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import "./About.scss";
+import AnimatedLetters from "../components/animation/AnimatedLetters";
+import AnimateReact from "../components/animation/AnimateReact";
+import AnimateStar from "../components/animation/AnimateStar";
 const About = () => {
   return (
     <div>
-      <Sidebar />
-      <AnimatedLetters text="About" />
+      <div className="about-page">
+        <Sidebar />
+        <div className="about-container">
+          <div className="about__page-description">
+            <p className="about_container-description">
+              I am hardworking and eager to learn person having clear goals and
+              a road map to achieve them, who works tirelessly for every goal i
+              set. <br />
+              I've been passionate about programming since childhood and I can't
+              imagine my life without writing code even a day. My main life
+              motto is work hard in silence let your success make the noise
+            </p>
+            <AnimatedLetters text="About" />
+          </div>
+          <AnimateStar />
+          <AnimateReact />
+        </div>
+      </div>
     </div>
   );
 };
