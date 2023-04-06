24fae2d04bb343b3a9433b697f9ac380687264b4
AnarMikailov <a.v.mikayilov@gmail.com>
2023-04-06 22:11:51 +0400

    skillbar

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,20 +1,26 @@
-import React, { useState, useEffect } from "react";
-import AnimatedLetters from "../components/animation/AnimatedLetters";
-import Sidebar from "../components/Sidebar";
-import SkillBar from "../components/SkillBar";
-import "./Skills.scss";
+import React, { useState, useEffect } from 'react';
+import AnimatedLetters from '../components/animation/AnimatedLetters';
+import Sidebar from '../components/Sidebar';
+import SkillBar from '../components/SkillBar';
+import './Skills.scss';
 const Skills = () => {
   return (
     <div>
       <Sidebar />
-      <AnimatedLetters text="Skills" />
       <div className="container-skill">
-        {/* <SkillBar skillText="HTML" value={85} /> */}
-        <SkillBar skillText="CSS" value={80} />
-        <SkillBar skillText="Javascript" value={30} />
-        <SkillBar skillText="React" value={100} />
-        <SkillBar skillText="React" value={10} />
-        <SkillBar skillText="React" value={60} />
+        <div>
+          <AnimatedLetters text="Skills" />
+        </div>
+        <div className="skillbar-container">
+          <SkillBar skillText="HTML" value={80} />
+          <SkillBar skillText="CSS" value={75} />
+          <SkillBar skillText="Javascript" value={65} />
+          <SkillBar skillText="React" value={65} />
+          <SkillBar skillText="Sass" value={50} />
+          <SkillBar skillText="Responsive design" value={60} />
+          <SkillBar skillText="Tailwind CSS" value={30} />
+          <SkillBar skillText="Botsrtap" value={20} />
+        </div>
       </div>
     </div>
   );
