b275047f7c1ec6ed484e70fd4b688a9223bc8525
AnarMikailov <a.v.mikayilov@gmail.com>
2023-04-13 18:00:25 +0400

    contact form

--- src/data/data.js
+++ src/data/data.js
@@ -1,15 +1,15 @@
 const project = {
   description1:
-    "  Fully responsive modern Ecommerce site with payments ability to add and edit products with using content management system. it is also integrated  stripe with you can cover real payments.File based Routing, Server-Side Rendering and much more.",
+    "  Fully responsive modern Ecommerce site with payments ability to add and edit products with using content management system. it is also integrated  stripe with you can cover real payments.File based Routing, Server-Side Rendering and much more. ",
 
   description2:
     "Fully responsive Admin Dashboard website that has been built using advanced concepts like React Hooks, React Router v6, Tailwind CSS, Syncfusion, and the React Context API for state management. It includes one dashboard, 3 pages, 4 apps, and 7 fully functional charts.",
 
   description3:
-    " Modern and Fully responsive GPT3 website covering main concepts like React functional components,React Hooks, Css BEM model, gradients, soft and pleasant animations ,media queries, Netlify for deployment  and much more.  ",
+    " Modern and Fully responsive GPT3 website covering main concepts like React functional components,React Hooks, Css BEM model, gradients, soft and pleasant animations ,media queries, Netlify for deployment  and much more.",
 
   description4:
-    "Gericht is a fully responsive restaurant website that has been built using modern technologies like React functional components,React Hooks, Fundamental CSS properties like flex and grid,  the BEM CSS Model and much more.  ",
+    "Gericht is a fully responsive restaurant website that has been built using modern technologies like React functional components,React Hooks, Fundamental CSS properties like flex and grid,  the BEM CSS Model and much more.",
 
   description5:
     "Dice Game is a fun game built with Vanilla JavaScript where players roll two dice and guess the sum of the two numbers rolled. It's a simple game that requires some strategy and luck to win. The interface is intuitive and easy to understand.",
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,13 +1,49 @@
-import React from 'react';
-import AnimatedLetters from '../components/animation/AnimatedLetters';
-import Sidebar from '../components/Sidebar';
-import './Contacts.scss';
+import React from "react";
+import AnimatedLetters from "../components/animation/AnimatedLetters";
+import Sidebar from "../components/Sidebar";
+import "./Contacts.scss";
 const Contacts = () => {
   return (
-    <div>
+    <>
       <Sidebar />
-      <AnimatedLetters text="Contact" />
-    </div>
+      <div className="contact-page">
+        <AnimatedLetters text="Contact" />
+        <div className="contact__page-form">
+          <h2 className="contact__page-title">Contact me</h2>
+
+          <form>
+            <div className="contact__page-name_email_container">
+              <input
+                className="contact__page-name_input"
+                type="text"
+                placeholder="Name"
+                required
+              />
+              <input
+                className="contact__page-email_input"
+                type="email"
+                placeholder="Email"
+                required
+              />
+            </div>
+            <input
+              className="contact__page-subject_input"
+              type="text"
+              required
+              placeholder="Subject"
+            />
+            <textarea
+              className="contact__page-textarea"
+              placeholder="Message.."
+              rows="6"
+            ></textarea>
+            <button className="btn contact__page-submit" type="submit">
+              Send
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
